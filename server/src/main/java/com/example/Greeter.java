a208bead66ebe1e2fec02cb1b47e072801026c48
Matheus S. Fontana <msfontana.dev@gmail.com>
2018-11-24 03:49:25 -0200

    Removing TODO + adding final signature to the method + Java Doc

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,14 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(final String someone) {
+  
+  /**
+   * This is a method.
+   * 
+   * @param person name
+   * @return Greeting String
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
